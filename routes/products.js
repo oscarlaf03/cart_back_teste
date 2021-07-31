62bbf08c00168affbc1205b86ae39237924d46e4
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-07-31 13:45:19 -0300

    routes splitted but testing

---
+++ routes/products.js
@@ -0,0 +1,22 @@
+
+
+
+const fs = require('fs');
+const path = require('path');
+const PRODUCT_DATA_FILE = path.join(__dirname, 'server-product-data.json');
+
+
+
+module.exports = (app) => {
+
+
+  app.get('/products', (req, res) => {
+    fs.readFile(PRODUCT_DATA_FILE, (err, data) => {
+      console.log('loggin data \n\n\n', data,'\n\n\n');
+      console.log(typeof data);
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(JSON.parse(data));
+    });
+  });
+
+}
