0d43a1595c7b61f74cd40877a05f32f6a68ad60f
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-08-01 00:33:09 -0300

    rendering orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const PRODUCT_DATA_FILE = path.join(__dirname, '../server-product-data.json');
-
-
+const ORDER_DATA_FILE = path.join(__dirname, '../server-order-data.json');
 
 module.exports = (app) => {
 
-
-  app.get('/products', (req, res) => {
-    fs.readFile(PRODUCT_DATA_FILE, (err, data) => {
+  app.get('/orders', (req, res) => {
+    fs.readFile(ORDER_DATA_FILE, (err, data) => {
       res.setHeader('Cache-Control', 'no-cache');
       res.json(JSON.parse(data));
     });
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const cart_routes = require('./routes/cart');
 const products_routes = require('./routes/products');
+const orders_routes = require('./routes/orders');
 
 
 app.set('port', (process.env.PORT || 3000));
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 
 cart_routes(app);
 products_routes(app);
+orders_routes(app);
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
