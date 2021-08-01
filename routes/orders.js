2f03da35db396f3b5e73705e27404b1643044487
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-08-01 16:42:08 -0300

    creates a new order

--- routes/orders.js
+++ routes/orders.js
@@ -11,4 +11,19 @@ module.exports = (app) => {
     });
   });
 
+  app.post('/orders', (req, res)=>{
+    console.log('\n\n**input order**\n\n', req.body);
+    fs.readFile(ORDER_DATA_FILE, (err,data) => {
+      const orders = JSON.parse(data);
+      new_order = { id: orders.length + 1, ...req.body}
+      orders.push(new_order)
+      console.log('\n\n**orders after push **\n\n', orders);
+
+      fs.writeFile(ORDER_DATA_FILE, JSON.stringify(orders,null,4),() => {
+        res.setHeader('Cache-Control', 'no-cache');
+        res.status(200)
+      });
+    });
+  });
+
 }
